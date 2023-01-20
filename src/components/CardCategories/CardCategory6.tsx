0d310cd7abaffc0026dcfe411da14eaf079ac4b5
Ryan <93636762+ccodine@users.noreply.github.com>
2023-01-20 14:29:00 +0700

    favico update

--- src/components/CardCategories/CardCategory6.tsx
+++ src/components/CardCategories/CardCategory6.tsx
@@ -2,6 +2,7 @@ import React, { FC } from "react";
 import NcImage from "../../shared/NcImage/NcImage";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
+import Image from "next/image";
 
 export interface CardCategory6Props {
   className?: string;
@@ -32,7 +33,7 @@ const CardCategory6: FC<CardCategory6Props> = ({
 
         <div className="absolute inset-5 flex flex-col justify-between items-center">
           <div className="flex justify-center items-center">
-            <NcImage
+            <Image
               src={featuredImage}
               containerClassName={`w-20 h-20 rounded-full overflow-hidden z-0 ${color}`}
             />
